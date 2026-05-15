cb3a70045627c8425eff07df11355c726d697d9f
kabincs9 <kabinbaniya9@gmail.com>
2026-05-15 22:04:44 +0545

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask
+#Flask, turn this file into a web application
+#and give me a variable called app via which to reference it.
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+
+#This particular function is not going to take any arguments,
+#so all it's going to do is return a string or str of text, hello comma world
+
+@app.route("/")
 def index():
-    if request.method == "POST":
-        # to use jinja for name like if there is value of name in greet
-        return render_template("greet.html", name=request.form.get("name"))
-    return render_template("index.html")
+    return "hello, world"
+# this is just some relatively little Python code,
+#albeit using a couple of new features, a new library
+#now
+# new file Let me go ahead and run code of requirements.txt.
+#And in this file, I'm going to simply specify what are all of the libraries
+#that I want this web application to use
+# ; not this index(): and double _ and _ means __
