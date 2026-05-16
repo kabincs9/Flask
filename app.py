275609b065a4226563341d2e7fa60fd9ec7904f3
kabincs9 <kabinbaniya9@gmail.com>
2026-05-16 22:51:16 +0545

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-from flask import Flask
-#Flask, turn this file into a web application
-#and give me a variable called app via which to reference it.
+from flask import Flask, render_template, redirect, request, session
+from flask_session import Session
 
 app = Flask(__name__)
 
-
-#This particular function is not going to take any arguments,
-#so all it's going to do is return a string or str of text, hello comma world
+# to Configure session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
-    return "hello, world"
-# this is just some relatively little Python code,
-#albeit using a couple of new features, a new library
-#now
-# new file Let me go ahead and run code of requirements.txt.
-#And in this file, I'm going to simply specify what are all of the libraries
-#that I want this web application to use
-# ; not this index(): and double _ and _ means __
+    return render_template("index.html", name=session.get("name"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session["name"] = request.form.get("name")
+        return redirect("/")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear() # once you logout back to ...home page again
+    return redirect("/")
